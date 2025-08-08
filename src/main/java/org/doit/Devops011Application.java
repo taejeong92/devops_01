33bcd052efb88bd7dc0ff3a94d5fc77d3f03c763
tae jeong <dalamy@naver.com>
2025-08-08 14:17:45 +0900

    first commit

---
+++ src/main/java/org/doit/Devops011Application.java
@@ -0,0 +1,13 @@
+package org.doit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops011Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/doit/Devops011ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
